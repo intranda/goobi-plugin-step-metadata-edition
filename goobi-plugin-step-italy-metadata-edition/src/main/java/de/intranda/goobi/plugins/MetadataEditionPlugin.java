44c7d0ecb96d29df0c67b2796bca62f7a9a9e42e
Robert Sehr <robert.sehr@intranda.com>
2020-04-15 09:38:10 +0200

    initial commit

---
+++ goobi-plugin-step-italy-metadata-edition/src/main/java/de/intranda/goobi/plugins/MetadataEditionPlugin.java
@@ -0,0 +1,116 @@
+package de.intranda.goobi.plugins;
+
+import java.util.HashMap;
+
+import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.configuration.reloading.FileChangedReloadingStrategy;
+import org.apache.commons.configuration.tree.xpath.XPathExpressionEngine;
+import org.goobi.beans.Step;
+import org.goobi.production.enums.PluginGuiType;
+import org.goobi.production.enums.PluginReturnValue;
+import org.goobi.production.enums.PluginType;
+import org.goobi.production.enums.StepReturnValue;
+import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
+
+import de.sub.goobi.config.ConfigPlugins;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+
+@Log4j2
+@PluginImplementation
+public class MetadataEditionPlugin implements IStepPluginVersion2 {
+
+    @Getter
+    private Step step;
+    private String returnPath;
+
+    @Getter
+    private String title = "intranda_step_rename-files";
+    @Getter
+    private PluginType type = PluginType.Step;
+
+    @Getter
+    private PluginGuiType pluginGuiType = PluginGuiType.PART;
+
+    @Override
+    public PluginReturnValue run() {
+
+        return PluginReturnValue.FINISH;
+    }
+
+    @Override
+    public String cancel() {
+        return returnPath;
+    }
+
+    @Override
+    public boolean execute() {
+        if (run().equals(PluginReturnValue.FINISH)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String finish() {
+        return returnPath;
+    }
+
+    @Override
+    public String getPagePath() {
+        return null;
+    }
+
+    @Override
+    public void initialize(Step step, String returnPath) {
+        this.step = step;
+        this.returnPath = returnPath;
+
+        String projectName = step.getProzess().getProjekt().getTitel();
+
+        XMLConfiguration xmlConfig = ConfigPlugins.getPluginConfig(getTitle());
+        xmlConfig.setExpressionEngine(new XPathExpressionEngine());
+        xmlConfig.setReloadingStrategy(new FileChangedReloadingStrategy());
+
+        SubnodeConfiguration myconfig = null;
+
+        // order of configuration is:
+        // 1.) project name and step name matches
+        // 2.) step name matches and project is *
+        // 3.) project name matches and step name is *
+        // 4.) project name and step name are *
+        try {
+            myconfig = xmlConfig.configurationAt("//config[./project = '" + projectName + "'][./step = '" + step.getTitel() + "']");
+        } catch (IllegalArgumentException e) {
+            try {
+                myconfig = xmlConfig.configurationAt("//config[./project = '*'][./step = '" + step.getTitel() + "']");
+            } catch (IllegalArgumentException e1) {
+                try {
+                    myconfig = xmlConfig.configurationAt("//config[./project = '" + projectName + "'][./step = '*']");
+                } catch (IllegalArgumentException e2) {
+                    myconfig = xmlConfig.configurationAt("//config[./project = '*'][./step = '*']");
+                }
+            }
+        }
+
+        initConfig(myconfig);
+    }
+
+    private void initConfig(SubnodeConfiguration config) {
+
+    }
+
+    @Override
+    public HashMap<String, StepReturnValue> validate() {
+        return null;
+    }
+
+    @Override
+    public int getInterfaceVersion() {
+        return 0;
+    }
+
+}
